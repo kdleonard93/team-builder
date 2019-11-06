cd0fcbf5b78942c9da847ffba66fb7616f0967dc
Kyle Leonard <kdleonard@icloud.com>
2019-11-05 20:21:03 -0600

    Corrections
    still get a .map error

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
-import From from "./components/Form";
+import Form from "./components/Form";
 import MemberList from "./components/MemberList";
 
 function App() {
   return (
     <div className="App">
-      <From></From>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form />
+      <MemberList />
     </div>
   );
 }
